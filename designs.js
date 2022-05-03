b207a87a5958ac53834b2e40fa2f977f3a24de77
mbajgain <mariabajgain@gmail.com>
2022-05-03 10:43:03 -0400

    final version for review

--- designs.js
+++ designs.js
@@ -1,6 +1,35 @@
 const canvas = document.getElementById("pixelCanvas"); 
 const sizePicker = document.getElementById("sizePicker");
-const button = document.getElementById("submit")
+const button = document.getElementById("submit");
+
+// Code to select and store color
+var userColor;
+var defaultColor = "#000000";
+
+window.addEventListener("load", startup, false);
+
+function startup() {
+    userColor = document.querySelector("#colorPicker");
+    userColor.value = defaultColor;
+    userColor.addEventListener("input", updateFirst, false);
+    userColor.addEventListener("change", updateAll, false);
+    userColor.select();
+  }
+
+function updateFirst(event) {
+  var p = document.querySelector("td");
+
+  if (p) {
+    p.style.color = event.target.value;
+  }
+}
+
+// Code to update color
+function updateAll(event) {
+    document.querySelectorAll("td").forEach(function(p) {
+      p.style.color = event.target.value;
+    });
+  }
 
 
 function makeGrid() {
@@ -20,14 +49,18 @@ function makeGrid() {
         for (let x = 1; x <= xAxis; x++) {
             let cell = document.createElement('td');
             row.appendChild(cell);
-            // In case you need to put a div in each cell ...
-            // let pixel = document.createElement('div');
-            // cell.appendChild(pixel);
+            // Adds color to cells upon selecting cells
+            cell.addEventListener('mousedown', function() {
+                const userColor = document.getElementById("colorPicker").value;
+                this.style.backgroundColor = userColor;
+            });  
         }
     }
+    
 }
- 
+// Function ensures "submit" does not refresh page, but populates grid
 document.getElementById("sizePicker").addEventListener("submit", function(grid) {
     grid.preventDefault();
     makeGrid();
   });
+
